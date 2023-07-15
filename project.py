dc81138b8ebfd271491976fd4401ff0d43a029a1
stjepan.tadic <stjepan.tadich@gmail.com>
2023-07-15 20:17:12 +0100

    Line indicators and their functionality added, improved design of some widgets and their looks

--- project.py
+++ project.py
@@ -38,19 +38,33 @@ class SlotMachine(tk.Tk):
         bg_label.place(x=0, y=0)
 
         # Message displaying information during the game
-        msg_lbl = ttk.Label(self, text='Welcome! Spin to start a game.', font=('Arial', 18), foreground='white', background='black', width=35, anchor='center')
-        msg_lbl.grid(row=0, column=0, columnspan=3, pady=10)
-
+        msg_lbl = ttk.Label(self, text='Welcome to Dino Hunt! Spin to start.', font=('Arial', 18), foreground='white', background='blue', width=35, anchor='center', relief='groove', borderwidth=10)
+        msg_lbl.grid(row=0, column=1, columnspan=3, pady=10)
+        
+        # Paylines played (line indicator) widgets
+        self.line1 = ttk.Label(self, text='Line 1', foreground='white', background='green', font=('', 18), relief='raised', borderwidth=5)
+        self.line2 = ttk.Label(self, text='Line 2', foreground='white', background='red', font=('', 18), relief='sunken', borderwidth=5)
+        self.line3 = ttk.Label(self, text='Line 3', foreground='white', background='red', font=('', 18), relief='sunken', borderwidth=5)
+        self.line11 = ttk.Label(self, text='Line 1', foreground='white', background='green', font=('', 18), relief='raised', borderwidth=5)
+        self.line22 = ttk.Label(self, text='Line 2', foreground='white', background='red', font=('', 18), relief='sunken', borderwidth=5)
+        self.line33 = ttk.Label(self, text='Line 3', foreground='white', background='red', font=('', 18), relief='sunken', borderwidth=5)
+        self.line1.grid(row=2, column=0, padx=(10, 0))
+        self.line2.grid(row=1, column=0, padx=(10, 0))
+        self.line3.grid(row=3, column=0, padx=(10, 0))
+        self.line11.grid(row=2, column=4, padx=(0, 10))
+        self.line22.grid(row=1, column=4, padx=(0, 10))
+        self.line33.grid(row=3, column=4, padx=(0, 10))
+        
         # Slot machine's frame encompasing reels (slots)
-        slot_frm = ttk.Frame(self, relief='sunken', borderwidth=5)
-        slot_frm.grid(row=1, column=0, columnspan=3, padx=10)
+        slot_frm = ttk.Frame(self, relief='sunken', borderwidth=10)
+        slot_frm.grid(row=1, column=1, rowspan=3, columnspan=3, padx=10)
 
 
 
         class Slot:
             '''A class representing slot for a symbol'''
             def __init__(self, container):
-                self.canvas = tk.Canvas(container, relief='groove', borderwidth=2, width=150, height=150)
+                self.canvas = tk.Canvas(container, relief='groove', borderwidth=6, width=150, height=150)
                 
         # Create slots for symblos
         self.slot_1x1 = Slot(slot_frm)
@@ -76,16 +90,16 @@ class SlotMachine(tk.Tk):
         
         # Instance of the ControlFrame
         control_frame = self.ControlFrame()
-        control_frame.grid(row=2, column=0, columnspan=3)
+        control_frame.grid(row=4, column=1, columnspan=3, pady=10)
     
     
     
     class ControlFrame(ttk.Frame):
-        '''A class to represent controls of the slot machine'''
+        '''A class to represent controls of the slot machine and its functionality'''
         def __init__(self):
             super().__init__()
             
-            self.grid(columnspan=3, pady=10)
+            self.config(relief='raised', borderwidth=10)
             
             # Initalize IntVar to store bet value and set its default value to 1, create bet label and amount-adjust slider for bet
             self.bet_var = tk.IntVar()
@@ -104,15 +118,14 @@ class SlotMachine(tk.Tk):
             self.total_var.set(1)
             self.total_lbl = ttk.Label(self, text=f'Total Bet: ${self.total_var.get()}', font=('', 16), justify='right', foreground='red', width=11)
 
-            # Balance
+            # Balance widget
             self.balance_var = tk.IntVar()
             self.balance_var.set(BALANCE)
             self.balance_lbl = ttk.Label(self, text=f'BALANCE: ${self.balance_var.get()}', font=('', 16))
             
             # Buttons widgets and thier appearance
-            self.spin_btn = ttk.Button(self, text='SPIN', command=self.spin)
-            self.cashout_btn = ttk.Button(self, text='Cash Out', command=self.cashout_menu)
-            ttk.Style().configure('TButton', font=('Helvetica', 20))
+            self.spin_btn = tk.Button(self, text='SPIN', font=('', 16, 'bold'), width=6, height=3, relief='raised', bd=8, bg='dark green', fg='white', activebackground='green', activeforeground='white', command=self.spin)
+            self.cashout_btn = tk.Button(self, text='Cash Out', font=('', 16), relief='raised', bd=5, command=self.cashout_menu)
 
             # Grid layout for widgets inside of a control frame
             self.bet_lbl.grid(row=0, column=0, padx=(3, 0))
@@ -143,10 +156,16 @@ class SlotMachine(tk.Tk):
             
             
         def update_lines(self, *args):
-            '''Updates number of bet lines (paylines) and total'''
+            '''Updates number of bet lines (paylines), total, and line indicator'''
             self.paylines_lbl.config(text=f'Paylines: {self.payline_var.get()}')
             self.total_var.set(self.bet_var.get()*self.payline_var.get())
             self.total_lbl.config(text=f'Total Bet: ${self.total_var.get()}')
+            
+            # Lines indicator update using ternary conditional operator
+            game.line2.config(background='green' if self.payline_var.get() >= 2 else 'red', relief='raised' if self.payline_var.get() >= 2 else 'sunken')
+            game.line22.config(background='green' if self.payline_var.get() >= 2 else 'red', relief='raised' if self.payline_var.get() >= 2 else 'sunken')
+            game.line3.config(background='green' if self.payline_var.get() == 3 else 'red', relief='raised' if self.payline_var.get() == 3 else 'sunken')
+            game.line33.config(background='green' if self.payline_var.get() == 3 else 'red', relief='raised' if self.payline_var.get() == 3 else 'sunken')
             
             
         def spin_reels(self, lines):
@@ -157,13 +176,13 @@ class SlotMachine(tk.Tk):
             # Randomly generate 3 lists using nested list comprehension
             reels = [[random.choices(symbols, probability)[0] for _ in range(3)] for _ in range(3)]
            
-            # Display randomly choosen symbol on the corresponding slot of the reel
-            game.slot_1x1.canvas.create_image(75, 75, image=reels[0][0])
-            game.slot_1x2.canvas.create_image(75, 75, image=reels[1][0])
-            game.slot_1x3.canvas.create_image(75, 75, image=reels[2][0])
-            game.slot_2x1.canvas.create_image(75, 75, image=reels[0][1])
-            game.slot_2x2.canvas.create_image(75, 75, image=reels[1][1])
-            game.slot_2x3.canvas.create_image(75, 75, image=reels[2][1])
+            # Display randomly choosen symbol on the corresponding slot of the reel (line 1 set to the middle row, line 2 top row, lin 3 bottom row)
+            game.slot_1x1.canvas.create_image(75, 75, image=reels[0][1])
+            game.slot_1x2.canvas.create_image(75, 75, image=reels[1][1])
+            game.slot_1x3.canvas.create_image(75, 75, image=reels[2][1])
+            game.slot_2x1.canvas.create_image(75, 75, image=reels[0][0])
+            game.slot_2x2.canvas.create_image(75, 75, image=reels[1][0])
+            game.slot_2x3.canvas.create_image(75, 75, image=reels[2][0])
             game.slot_3x1.canvas.create_image(75, 75, image=reels[0][2])
             game.slot_3x2.canvas.create_image(75, 75, image=reels[1][2])
             game.slot_3x3.canvas.create_image(75, 75, image=reels[2][2])
@@ -171,7 +190,6 @@ class SlotMachine(tk.Tk):
             # Compare symbols in appropriate positions by using all() function which returns true if all items in iterable are true
             for line in range(lines):
                 if all(reels[i][line] == reels[j][line] for i, j in [(0, 1), (1, 2)]):
-                    print(f'LINE {line+1}')
                     # Winnings (multipliers per symbol)
                     match reels[0][line]:
                         case game.a:
@@ -214,6 +232,7 @@ class SlotMachine(tk.Tk):
                     self.balance_lbl.config(text=f'Balance: ${self.balance_var.get()}')
                 else:
                     self.quit()
+            
                     
 
 
